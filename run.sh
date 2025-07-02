2d1582bb497841561363e1def26d578b1ef95b74
SoulShadow8326 <soulshadow8326@gmail.com>
2025-07-02 17:45:28 +0530

    commented out

--- run.sh
+++ run.sh
@@ -4,17 +4,17 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-cd frontend
-npm install
-if [ $? -ne 0 ]; then
-  echo "npm install failed"
-  exit 1
-fi
-npm run build
-if [ $? -ne 0 ]; then
-  echo "Frontend build failed"
-  exit 1
-fi
+#cd frontend
+#npm install
+#if [ $? -ne 0 ]; then
+#  echo "npm install failed"
+#  exit 1
+#fi
+#npm run build
+#if [ $? -ne 0 ]; then
+#  echo "Frontend build failed"
+#  exit 1
+#fi
 
-cd ../.
+#cd ../.
 uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
